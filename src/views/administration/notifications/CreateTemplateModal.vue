ff0297961fc7c7244312ee78606e94f9c12a9557
Marlon Gaethje <mge@mm-software.com>
2024-01-05 16:39:21 +0100

    initial commit
    Signed-off-by: Marlon Gaethje <mge@mm-software.com>

--- src/views/administration/notifications/CreateTemplateModal.vue
+++ src/views/administration/notifications/CreateTemplateModal.vue
@@ -35,6 +35,36 @@
       label-class="required">
       <b-form-textarea id="input-5" v-model="template" class="required" rows="10" required trim />
     </b-form-group>
+
+    <b-form-group
+    id="fieldset-6"
+    :label = "this.$t('admin.scheduled_notification')"
+    label-for="input-6"
+    label-class="optional">
+      <input type="checkbox" id="input-6" v-model="schedulednotification" @click="expandView()"/>
+    </b-form-group>
+    <b-form-group v-if="schedulednotification"
+      id="fieldset-7"
+      :label="this.$t('admin.cron')"
+      label-for="input-7"
+      label-class="required">
+      <b-form-textarea id="input-7" v-model="cronExpression" class="required" required trim />
+    </b-form-group>
+    <b-form-group v-if="schedulednotification"
+      id="fieldset-8"
+      :label="this.$t('admin.email_destinations')"
+      label-for="input-8"
+      label-class="required">
+      <b-form-textarea id="input-8" v-model="emailDestinations" class="required" required trim />
+    </b-form-group>
+    <b-form-group v-if="schedulednotification"
+      id="fieldset-9"
+      :label="this.$t('admin.project_id')"
+      label-for="input-9"
+      label-class="required">
+      <b-form-textarea id="input-9" v-model="projectId" class="required" required trim />
+    </b-form-group>
+
     <template v-slot:modal-footer="{ cancel }">
       <b-button size="md" variant="secondary" @click="cancel()">{{ $t('message.close') }}</b-button>
       <b-button size="md" variant="primary" @click="createTemplate()">{{ $t('message.create') }}</b-button>
@@ -55,6 +85,9 @@
         this.description = template.description;
         this.mimeType = template.templateMimeType;
         this.template = template.template;
+        this.schedulednotification = template.schedulednotification;
+        this.emailDestinations = template.emailDestinations;
+        this.projectId = template.projectId;
       });
       this.$root.$on('bv::modal::hide', (_, modalId) => {
         if(modalId == 'createTemplateModal') {
@@ -68,12 +101,33 @@
         publisherClass: null,
         description: null,
         mimeType: null,
-        template: null
+        template: null,
+        schedulednotification: null,
+        cronExpression: null,
+        projectId: null
       }
     },
     methods: {
       createTemplate: function() {
-        let url = `${this.$api.BASE_URL}/${this.$api.URL_NOTIFICATION_PUBLISHER}`;
+        if(this.schedulednotification){
+          let url = `${this.$api.BASE_URL}/${this.$api.URL_SCHEDULEDNOTIFICATION_CREATE}`;
+          this.axios.put(url, {
+            created: new Date(),
+            cronString: this.cronExpression,
+            nextExecution: null,
+            lastExecution: null,
+            destinations: this.emailDestinations,
+            projectId: this.projectId
+          }).then(() => {
+            this.$emit('refreshTable');
+            this.$toastr.s(this.$t('admin.template_created'));
+          }).catch(() => {
+            this.$toastr.w(this.$t('condition.unsuccessful_action'));
+          });
+          this.$root.$emit('bv::hide::modal', 'createTemplateModal');
+
+        }else{
+          let url = `${this.$api.BASE_URL}/${this.$api.URL_NOTIFICATION_PUBLISHER}`;
         this.axios.put(url, {
           name: this.name,
           description: this.description,
@@ -88,6 +142,7 @@
           this.$toastr.w(this.$t('condition.unsuccessful_action'));
         });
         this.$root.$emit('bv::hide::modal', 'createTemplateModal');
+        }
       },
       resetValues: function () {
         this.name = null;
@@ -95,6 +150,13 @@
         this.description = null;
         this.mimeType = null;
         this.template = null;
+        this.schedulednotification = null;
+        this.cronExpression = null;
+        this.emailDestinations = null;
+        this.projectId = null;
+      },
+      expandView: function () {
+        
       }
     }
   }
